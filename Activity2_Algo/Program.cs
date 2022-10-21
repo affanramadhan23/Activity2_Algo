ca95b63233dc708402159bff91634633193035e8
affanramadhan23 <affannabilramadhan@gmail.com>
2022-10-21 16:55:18 +0700

    pembuatan namespace quicksort dan classnya

--- Activity2_Algo/Program.cs
+++ Activity2_Algo/Program.cs
@@ -1,1 +1,22 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickSort
+{
+    class Program
+    {
+        //array of integers to hold values
+        private int[] arr = new int[20];
+        private int cmp_count = 0; //number of comparison
+        private int mov_count = 0; //number of data movements
+
+        //Numbers of elements in array
+        private int n;
+
+
+
+    }
+}
